eb94d4128c9fed9c3d8bb408c575a66d61b16178
Leonardo Errati <64229723+PlasmaStark@users.noreply.github.com>
2022-10-04 16:45:09 +0200

    Update code.py

--- code.py
+++ code.py
@@ -6,10 +6,16 @@ import pandas as pd
 import math
 from random import choice
 
+# import data from a .ods file having this format:
+# - column 1 : names
+# - header : yes (1 row)
+# - no other data
+# return a list of all partecipants
 def importData(path):
     df = pd.read_excel(path, engine="odf")
     return df.values.tolist()
 
+# choose at random an adequate number of people
 def choosePeople(number,data):
     chosen = list()
     data2 = data.copy()
@@ -19,6 +25,7 @@ def choosePeople(number,data):
         chosen.append(P)
     return chosen
 
+# find the adequate number of people to return (empirical!)
 def magicNumber(data,factor):
     return 1+math.floor(len(data)/factor)
 
